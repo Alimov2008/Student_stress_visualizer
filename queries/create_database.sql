c2973629f1cb30a301dd4d307f3f4897cfc05961
Ali <alimovmuhammadali01222008@gmail.com>
2025-08-24 16:37:55 +0500

    added more info into README.md

---
+++ queries/create_database.sql
@@ -0,0 +1,11 @@
+CREATE TABLE stress_data (
+    timestamp TIMESTAMP,
+    academic_stage VARCHAR(50),
+    peer_pressure INT,
+    home_academic_pressure INT,
+    study_environment VARCHAR(50),
+    coping_strategy TEXT,
+    bad_habits VARCHAR(10),
+    academic_competition INT,
+    stress_index INT
+);
